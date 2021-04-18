16ed97d6617ff0d19d08092719aeb1c2dcdf22d8
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2021-04-18 12:37:31 +0700

    add fake data for auth pages

---
+++ src/app/modules/auth/_mocks_/userTableMock.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    username: "admin",
+    password: "demo",
+    email: "admin@demo.com",
+    accessToken: "access-token-8f3ae836da744329a6f93bf20594b5cc",
+  },
+  {
+    id: 2,
+    username: "user",
+    password: "demo",
+    email: "user@demo.com",
+    accessToken: "access-token-6829bba69dd3421d8762-991e9e806dbf",
+  },
+  {
+    id: 3,
+    username: "guest",
+    password: "demo",
+    email: "guest@demo.com",
+    accessToken: "access-token-d2dff7b82f784de584b60964abbe45b9",
+  }
+];
