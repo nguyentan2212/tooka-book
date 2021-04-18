6dcfa820d86adbdbfba29944650c3ffc505d47e6
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2021-04-18 12:35:14 +0700

    add redirect exact to my-page

--- src/app/BasePage.js
+++ src/app/BasePage.js
@@ -2,12 +2,15 @@ import React, {Suspense} from "react";
 import { Redirect, Switch, Route } from "react-router-dom";
 import { ContentRoute } from "../template/layout/components/content/ContentRoute";
 import { LayoutSplashScreen } from '../template/layout/core/MetronicSplashScreen';
-import { MyPage } from "./pages/MyPage";
+import { MyPage } from "./modules/MyPage";
 
 export default function BasePage() {
     return(
         <Suspense fallback={<LayoutSplashScreen />}>
             <Switch>
+                {
+                    <Redirect exact from="/" to="/my-page" />
+                }
             <ContentRoute path="/my-page" component={MyPage}></ContentRoute>
         </Switch>
         </Suspense>
