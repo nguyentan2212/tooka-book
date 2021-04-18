7cafa87d8287cd2eb335cf6cbb552f1a2e7dc92a
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2021-04-18 12:39:39 +0700

    add setup for axios and axios-mock-adapter

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from "axios";
 import './index.css';
 import App from './app/App';
 import './style.css';
 import {MetronicLayoutProvider} from './template/layout/core/MetronicLayout';
 import {MetronicSubheaderProvider} from './template/layout/core/MetronicSubheader';
 import {MetronicSplashScreenProvider} from './template/layout/core/MetronicSplashScreen';
+import {mockAxios ,setupAxios} from './app/js';
+import {getToken} from './app/js/utilities/token'
 
 import "./template/assets/plugins/keenthemes-icons/font/ki.css";
 import "socicon/css/socicon.css";
@@ -14,6 +17,12 @@ import "./template/assets/plugins/flaticon/flaticon.css";
 import "./template/assets/plugins/flaticon2/flaticon.css";
 // Datepicker
 import "react-datepicker/dist/react-datepicker.css";
+
+// fake api
+mockAxios(axios);
+
+//setup  axios requests 
+setupAxios(axios,getToken);
 
 ReactDOM.render(
   <MetronicLayoutProvider>
