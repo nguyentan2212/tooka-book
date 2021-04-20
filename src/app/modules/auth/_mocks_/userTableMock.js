23cf3bffc704f17a682bab41c982375720ca2749
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2021-04-20 13:24:15 +0700

    add fullname property for user objects

--- src/app/modules/auth/_mocks_/userTableMock.js
+++ src/app/modules/auth/_mocks_/userTableMock.js
@@ -2,6 +2,7 @@ export default [
   {
     id: 1,
     username: "admin",
+    fullname:"admin is admin",
     password: "demo",
     email: "admin@demo.com",
     accessToken: "access-token-8f3ae836da744329a6f93bf20594b5cc",
@@ -9,6 +10,7 @@ export default [
   {
     id: 2,
     username: "user",
+    fullname: "user is user",
     password: "demo",
     email: "user@demo.com",
     accessToken: "access-token-6829bba69dd3421d8762-991e9e806dbf",
@@ -16,6 +18,7 @@ export default [
   {
     id: 3,
     username: "guest",
+    fullname: "guest is guest",
     password: "demo",
     email: "guest@demo.com",
     accessToken: "access-token-d2dff7b82f784de584b60964abbe45b9",
