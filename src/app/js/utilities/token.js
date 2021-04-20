0b11492434b4682137a827de4d97ecef9695055a
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2021-04-20 13:23:47 +0700

    change from sessionStorage to localStorage, add removeToken function

--- src/app/js/utilities/token.js
+++ src/app/js/utilities/token.js
@@ -1,9 +1,13 @@
 export function saveToken(userToken) {
-    sessionStorage.setItem('token', JSON.stringify(userToken));
+  localStorage.setItem('token', JSON.stringify(userToken));
   }
   
 export function getToken() {
-    const tokenString = sessionStorage.getItem('token');
+    const tokenString = localStorage.getItem('token');
     const userToken = JSON.parse(tokenString);
-    return userToken?.token
+    return userToken;
+}
+
+export function removeToken() {
+  localStorage.removeItem('token');
 }
