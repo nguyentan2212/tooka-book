3333ca61b48d83833130d3fd11ad7ba8af4fc25f
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2021-04-20 13:24:51 +0700

    fix route logic, add logout route

--- src/app/Routes.js
+++ src/app/Routes.js
@@ -4,23 +4,26 @@ import BasePage from './BasePage';
 import {AuthPage} from './modules/auth/pages/AuthPage';
 import {Layout} from '../template/layout/components/Layout';
 import useUser from './js/hooks/useUser';
+import{ getToken, removeToken} from './js/utilities/token';
+import Logout from './modules/auth/Logout';
 
 export function Routes() {
     const { user, setUser } = useUser();
+    
     console.log(user);
     return (
       <Switch>
         {/*Render auth page when user at `/auth` and not authorized.
            Otherwise redirect to root page (`/`)*/}
-        {!user ? (<Route><AuthPage setUser={user => setUser(user)} /></Route>) : (<Layout><BasePage /></Layout>)}
+        {!user ? (<Route><AuthPage setUser={user => setUser(user)} /></Route>) : (<Redirect from="/auth" to="/" />)}
   
-        
+        <Route path="/logout" component={() => <Logout logout={removeToken}></Logout>} />
 
         {/* <Route path="/error" component={ErrorsPage} />
             <Route path="/logout" component={Logout} /> */}
 
         {/*Redirect to `/auth` when user is not authorized*/}
-        {/*!token ? (<Redirect to="/auth/login" />) : (<Layout><BasePage /></Layout>)*/}
+        {!user ? (<Redirect to="/auth/login" />) : (<Layout appuser={user}><BasePage /></Layout>)}
       </Switch>
     );
   }
