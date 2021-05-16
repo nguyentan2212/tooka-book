ee78c76a0d71d2ffbdb567f7613fa50ec6972426
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2021-05-16 16:35:17 +0700

    update dashboard layout

--- src/app/modules/dashboard/Dashboard.js
+++ src/app/modules/dashboard/Dashboard.js
@@ -2,21 +2,36 @@ import React from "react";
 import { OverviewWiget } from "./components/OverviewWidget";
 import { RevenueChartWidget } from "./components/RevenueChartWidget";
 import { OrderChartWidget } from "./components/OrderChartWidget";
+import {TilesWidget} from "./components/TilesWidget";
 
 function Dashboard() {
   return (
     <div>
       <div className="row">
-        <div className="col-lg-6 col-xxl-6">
+        <div className="col-lg-6 col-xxl-4">
           <OverviewWiget className="card-stretch gutter-b" />
         </div>
-        <div className="col-lg-6 col-xxl-6">
+        <div className="col-lg-6 col-xxl-8">
           <div className="row">
-            <div className="col-lg-6 col-xxl-12">
-              <RevenueChartWidget className="card-stretch gutter-b" />
+            <div className="col-xxl-12">
+              <TilesWidget
+                className="card-stretch gutter-b"
+                baseColor="info"
+              />
             </div>
-            <div className="col-lg-6 col-xxl-12">
-              <OrderChartWidget className="card-stretch gutter-b" />
+          </div>
+          <div className="row">
+            <div className="col-lg-12 col-xxl-6">
+              <OrderChartWidget
+                className="card-stretch gutter-b"
+                baseColor="info"
+              />
+            </div>
+            <div className="col-lg-12 col-xxl-6">
+              <RevenueChartWidget
+                className="card-stretch gutter-b"
+                baseColor="primary"
+              />
             </div>
           </div>
         </div>
