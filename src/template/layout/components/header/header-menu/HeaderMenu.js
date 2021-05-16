18f06221df92cf152c3abf7703288ed2c1b0ca47
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2021-05-16 16:34:53 +0700

    replace dashboard link with store title

--- src/template/layout/components/header/header-menu/HeaderMenu.js
+++ src/template/layout/components/header/header-menu/HeaderMenu.js
@@ -2,31 +2,27 @@
 import React from "react";
 import { useLocation } from "react-router";
 import { NavLink } from "react-router-dom";
-import SVG from "react-inlinesvg";
-import { toAbsoluteUrl, checkIsActive } from "../../../../helpers";
+import { checkIsActive } from "../../../../helpers";
 
 export function HeaderMenu({ layoutProps }) {
-    const location = useLocation();
-    const getMenuItemActive = (url) => {
-        return checkIsActive(location, url) ? "menu-item-active" : "";
-    }
+  const location = useLocation();
+  const getMenuItemActive = (url) => {
+    return checkIsActive(location, url) ? "menu-item-active" : "";
+  };
 
-    return <div
-        id="kt_header_menu"
-        className={`header-menu header-menu-mobile ${layoutProps.ktMenuClasses}`}
-        {...layoutProps.headerMenuAttributes}
+  return (
+    <div
+      id="kt_header_menu"
+      className={`header-menu header-menu-mobile ${layoutProps.ktMenuClasses}`}
+      {...layoutProps.headerMenuAttributes}
     >
-        {/*begin::Header Nav*/}
-        <ul className={`menu-nav ${layoutProps.ulClasses}`}>
-            {/*begin::1 Level*/}
-            <li className={`menu-item menu-item-rel ${getMenuItemActive('/dashboard')}`}>
-                <NavLink className="menu-link" to="/dashboard">
-                    <span className="menu-text">Dashboard</span>
-                    {layoutProps.rootArrowEnabled && (<i className="menu-arrow" />)}
-                </NavLink>
-            </li>
-            {/*end::1 Level*/}
-        </ul>
-        {/*end::Header Nav*/}
-    </div>;
+      {/*begin::Header Nav*/}
+      <div className="d-flex align-items-center">
+        <NavLink to="/dashboard">
+          <h3 className="text-dark font-weight-bolder text-uppercase">Cửa hàng sách tooka book</h3>
+        </NavLink>
+      </div>
+      {/*end::Header Nav*/}
+    </div>
+  );
 }
